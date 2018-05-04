6dbafc4189d0ec9981f321fb6dd31adbef879f83
Oliver <oliverbecker@usp.br>
2018-05-04 00:42:27 -0300

    Função de leitura do arquivo de entrada concluída
    Alteração na operação 1, separando em uma função
    para a leitura do arquivo de entrada e outra para
    escrita do arquivo de saída.

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 
 typedef struct {
 	int codEscola;
-	char dataInicio[10];
-	char dataFinal[10];
+	char dataInicio[11];
+	char dataFinal[11];
 	int tamNome;
 	char *nomeEscola;
 	int tamMunicipio;
@@ -23,25 +23,94 @@ typedef struct {
 	int numElementos;
 } VETREGISTROS;
 
-void CarregaArquivo(char* nomeArquivo) {
+void FechaArquivoDeSaida(FILE* fp) {
+	char status = 1;
+	rewind(fp);
+	fwrite(&status, sizeof(status), 1, fp);
+	fclose(fp);
+}
+
+void CriaArquivoDeSaida() {
+	FILE* fp = fopen(ARQUIVO_SAIDA, "wb");
+	char status = 0;
+	int topoPilha = -1;
+	fwrite(&status, sizeof(status), 1, fp);
+	fwrite(&topoPilha, sizeof(topoPilha), 1, fp);
+	FechaArquivoDeSaida(fp);
+}
+
+void InsereRegistro(REGISTRO* registro) {
+}
+
+void CriaArquivoDeSaida(VETREGISTROS* vetRegistros) {
+}
+
+VETREGISTROS* LeituraArquivoDeEntrada(char* nomeArquivo) {
 	FILE* fp = fopen(nomeArquivo, "r");
 	if (fp == NULL) {
 		printf(ERRO_GERAL);
-		return;
+		return NULL;
 	}
-	REGISTRO registro;
-	
-	//while (fscanf(fp, "%d", &(registro->codEscola)) != EOF) {
-	for (int i = 0; i < 6; ++i) {
-		char* string = NULL;
-		int n;
-		//getline(&string, &n, fp);
-		//getdelim(&string, &n, ';', fp);
-		n = fscanf(fp, "%m[^;\n]%*c", &string);
-		printf("campo%d, n=%d, strlen=%2lu: %s\n", i, n, strlen(string), string);
-		free(string);
+
+	VETREGISTROS* vetRegistros = (VETREGISTROS*) calloc(1, sizeof(VETREGISTROS));
+	REGISTRO* registro;
+
+	int counter = 0;
+	char* string[6];
+
+	while (fscanf(fp, "%m[^;\n]", &string[0]) != EOF) {
+		for (int i = 1; i < 6; ++i)
+			fscanf(fp, "%*c%m[^;\n]", &string[i]);
+		fgetc(fp);
+
+		vetRegistros->vetRegistro = (REGISTRO**)realloc(vetRegistros->vetRegistro, 
+				sizeof(REGISTRO*) * (++vetRegistros->numElementos));
+
+		registro = (REGISTRO*)malloc(sizeof(REGISTRO));
+/*		for (int i = 0; i < 6; ++i)
+			printf(" %s |", string[i]);
+		printf("\n");
+*/
+		registro->codEscola = atoi(string[0]);
+		strcpy(registro->dataInicio, (string[4] != NULL) ? string[4] : "0000000000");
+		strcpy(registro->dataFinal, (string[5] != NULL) ? string[5] : "0000000000");
+		registro->tamNome = (string[1] != NULL) ? strlen(string[1]) : 0;
+		registro->nomeEscola = string[1];
+		registro->tamMunicipio = (string[2] != NULL) ? strlen(string[2]) : 0;
+		registro->municipio = string[2];
+		registro->tamEndereco = (string[3] != NULL) ? strlen(string[3]) : 0;
+		registro->endereco = string[3];
+
+		vetRegistros->vetRegistro[counter++] = registro;
+
+		free(string[0]);
+		if (string[4] != NULL) free(string[4]);
+		if (string[5] != NULL) free(string[5]);
 	}
+
+	REGISTRO* r;
+	for (int i = 0; i < vetRegistros->numElementos; ++i) {
+		r = vetRegistros->vetRegistro[i];
+		printf("%d %s %s %d %s %d %s %d %s\n", r->codEscola, r->dataInicio, r->dataFinal,
+			r->tamNome, r->nomeEscola, r->tamMunicipio, r->municipio, r->tamEndereco,
+			r->endereco);
+	}
+	//printf("tamanho do vetor de registros: %d\n", vetRegistros->numElementos);
+
+// free do vetor de registros
+/*	for (int i = 0; i < vetRegistros->numElementos; ++i) {
+		r = vetRegistros->vetRegistro[i];
+		if (r->nomeEscola != NULL) free(r->nomeEscola);
+		if (r->municipio != NULL) free(r->municipio);
+		if (r->endereco != NULL) free(r->endereco);
+		free(r);
+	}
+	free(vetRegistros->vetRegistro);
+	free(vetRegistros);
+*/
 	fclose(fp);
+
+	return vetRegistros;
 }
 
 VETREGISTROS* RecuperaTodosRegistros() {
@@ -95,7 +164,9 @@ int main(int argc, char *argv[]){
 	switch (atoi(argv[1])) {
 		case 1:
 			ConfereEntrada(argc, 3);
-			CarregaArquivo(argv[2]);
+			vetRegistros = LeituraArquivoDeEntrada(argv[2]);
+			CriaArquivoDeSaida();
+			InsereVetorDeRegistros(vetRegistros);
 			break;
 		case 2:
 			ConfereEntrada(argc, 2);
